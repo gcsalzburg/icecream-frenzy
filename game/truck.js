41f47990015e7593083374270d8ae3ea1fcac7b7
gcsalzburg <g.cave@kiska.com>
2019-01-04 14:34:29 +0100

    Change to fn prototype

--- game/truck.js
+++ game/truck.js
@@ -1,18 +1,20 @@
 
-var truck = {
-    src: null,
-    left: 20,
-    lane: 0,
-    render: function(){
-        ctx.drawImage(this.src,this.left,TRUCK_LANES_Y[this.lane]);
-    },
-    lane_change: function(dir){
-        this.lane = this.lane+dir;
-        if(this.lane<0){
-            this.lane = 0;
-        }else if(this.lane>=TRUCK_LANES_Y.length){
-            this.lane = TRUCK_LANES_Y.length-1;
-        }
 
+function Truck(){
+    this.src = null;
+    this.left = 20;
+    this.lane = 0;
+}
+
+Truck.prototype.render = function(){
+    ctx.drawImage(this.src,this.left,TRUCK_LANES_Y[this.lane]);
+}
+
+Truck.prototype.change_lane = function(dir){
+    this.lane = this.lane+dir;
+    if(this.lane<0){
+        this.lane = 0;
+    }else if(this.lane>=TRUCK_LANES_Y.length){
+        this.lane = TRUCK_LANES_Y.length-1;
     }
 }
