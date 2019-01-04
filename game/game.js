aa41e43b98073221a5fdf7050b28bf450b05546f
gcsalzburg <g.cave@kiska.com>
2019-01-04 14:02:58 +0100

    Add road speed and acceleration handling

--- game/game.js
+++ game/game.js
@@ -4,11 +4,65 @@ var game = {
     h: 375
 }
 
+var NUM_LANES = 3;
+var TRUCK_LANES_Y = [65,171];
+
+var truck = {
+    src: null,
+    left: 20,
+    lane: 0,
+    render: function(){
+        ctx.drawImage(this.src,this.left,TRUCK_LANES_Y[this.lane]);
+    },
+    lane_change: function(dir){
+        this.lane = this.lane+dir;
+        if(this.lane<0){
+            this.lane = 0;
+        }else if(this.lane>=TRUCK_LANES_Y.length){
+            this.lane = TRUCK_LANES_Y.length-1;
+        }
+
+    }
+}
+
+var road = {
+    src: null,
+
+    speed: 0,           // current speed in pixels per second
+    accel: 100,         // pixels per second^2
+    target_speed: 300,  // desired road speed
+
+    pos: 0,
+    render: function(){
+        // Draw road, and one road behind
+        // TODO: Programmatically generate this terrain
+        ctx.drawImage(this.src,this.pos,0);
+        ctx.drawImage(this.src,this.pos+this.src.width,0);
+    },
+    update: function(modifier){
+
+        // Accelerate to desired speed (if necessary)
+        if(this.target_speed > this.speed){
+            this.speed += this.accel*modifier;
+        }
+        if(this.speed > this.target_speed){
+            this.speed = this.target_speed;
+        }
+
+        // Advance position of road
+        this.pos -= this.speed*modifier;
+        if(this.pos < -this.src.width){
+            this.pos = this.pos + this.src.width;
+        }
+    }
+}
+
 // Queue image assets
 var ASS_MANAGER = new AssetManager();
 ASS_MANAGER.setDefaultPath("assets/");
 ASS_MANAGER.queueDownloads(
     'bg.png',
+    'truck.png',
     'cone-strawberry.png',
     'cone-vanilla.png'
 )
@@ -29,6 +83,7 @@ var monstersCaught = 0;
 
 // Handle keyboard controls
 var keysDown = {};
+var keysUp = {};
 
 // for fps measurement
 var times = [];
@@ -39,7 +94,8 @@ addEventListener("keydown", function (e) {
 }, false);
 
 addEventListener("keyup", function (e) {
-	delete keysDown[e.keyCode];
+    delete keysDown[e.keyCode];
+    keysUp[e.keyCode] = true;
 }, false);
 
 // Reset the game when the player catches a monster
@@ -52,8 +108,21 @@ var reset = function () {
 	monster.y = 32 + (Math.random() * (canvas.height - 64));
 };
 
+
 // Update game objects
 var update = function (modifier) {
+
+    if(38 in keysDown){         // UP
+        delete keysDown[38];
+        truck.lane_change(-1);
+    }else if(40 in keysDown){   // DOWN
+        delete keysDown[40];
+        truck.lane_change(1);
+    }
+
+    road.update(modifier);
+
+/*
 	if (38 in keysDown) { // Player holding up
 		hero.y -= hero.speed * modifier;
 	}
@@ -65,7 +134,7 @@ var update = function (modifier) {
 	}
 	if (39 in keysDown) { // Player holding right
 		hero.x += hero.speed * modifier;
-	}
+	}*/
 
 	// Are they touching?
 	if (
@@ -81,24 +150,18 @@ var update = function (modifier) {
 
 // Draw everything
 var render = function () {
-    ctx.drawImage(ASS_MANAGER.getAsset('bg.png'), 0, 0);
     ctx.drawImage(ASS_MANAGER.getAsset('cone-strawberry.png'), hero.x, hero.y);
     ctx.drawImage(ASS_MANAGER.getAsset('cone-vanilla.png'), monster.x, monster.y);
 
-	// Score
-	ctx.fillStyle = "rgb(250, 250, 250)";
-	ctx.font = "24px Helvetica";
-	ctx.textAlign = "left";
-	ctx.textBaseline = "top";
-    ctx.fillText("Goblins caught: " + monstersCaught, 32, 32);
+    road.render();
+    truck.render();
     
 	// FPS
 	ctx.fillStyle = "rgb(250, 250, 250)";
 	ctx.font = "8px Helvetica";
 	ctx.textAlign = "right";
 	ctx.textBaseline = "bottom";
-    ctx.fillText("FPS: " + fps, 802, 365);
-    
+    ctx.fillText("FPS: " + fps, 802, 365);    
 };
 
 // The main game loop
@@ -132,6 +195,10 @@ var then;
 // Let's play this game!
 // Load in assets
 ASS_MANAGER.downloadAll(function() {
+
+    road.src = ASS_MANAGER.getAsset('bg.png');
+    truck.src = ASS_MANAGER.getAsset('truck.png');
+
     then = Date.now();
     reset();
     main();
