6f1205254696f954dd21b7e1c6fa3faf21709f0a
gcsalzburg <g.cave@kiska.com>
2019-01-04 14:36:58 +0100

    Update to Road.js and Truck.js

--- game/game.js
+++ game/game.js
@@ -1,77 +1,26 @@
-// CORE VARS
-var game = {
-    w: 812,
-    h: 375
-}
-
 
 // Cross-browser support for requestAnimationFrame
 var w = window;
 requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
 
+// /////////////////////////////////
+// // CORE PROGRAM VARIABLES      //
+// // aka magic numbers           //
+// /////////////////////////////////
+
+var game = {
+    w: 812,
+    h: 375
+}
+
 var NUM_LANES = 3;
 var LANES_Y = [2,96,191];
 var TRUCK_LANES_Y = [65,171];
 
-var truck = {
-    src: null,
-    left: 20,
-    lane: 0,
-    render: function(){
-        ctx.drawImage(this.src,this.left,TRUCK_LANES_Y[this.lane]);
-    },
-    lane_change: function(dir){
-        this.lane = this.lane+dir;
-        if(this.lane<0){
-            this.lane = 0;
-        }else if(this.lane>=TRUCK_LANES_Y.length){
-            this.lane = TRUCK_LANES_Y.length-1;
-        }
 
-    }
-}
-
-var road = {
-    src: null,
-
-    is_starting_up: true,  // Are we doing the initial acceleration or not
-
-    speed: 200,           // current speed in pixels per second -> TODO: set to 0 for production
-    accel: 100,         // pixels per second^2
-    target_speed: 300,  // desired road speed
-
-    pos: 0,
-    render: function(){
-        // Draw road, and one road behind
-        // TODO: Programmatically generate this terrain
-        ctx.drawImage(this.src,this.pos,0);
-        ctx.drawImage(this.src,this.pos+this.src.width,0);
-    },
-    update: function(modifier){
-
-        // Accelerate to desired speed (if necessary)
-        if(this.target_speed > this.speed){
-            this.speed += this.accel*modifier;
-        }
-        if(this.speed > this.target_speed){
-            this.speed = this.target_speed;
-            if(this.is_starting_up){
-                // Finished starting up, so trigger something!
-                CUSTOMERS.add();
-                this.is_starting_up = false;
-            }
-        }
-
-        // Advance position of road
-        this.pos -= this.speed*modifier;
-        if(this.pos < -this.src.width){
-            this.pos = this.pos + this.src.width;
-        }
-    }
-}
-
-// Create customers array
-var CUSTOMERS = new Customers();
+// //////////////////////////////////
+// // ASSET & GAME OBJECTS LOADING //
+// //////////////////////////////////
 
 // Queue image assets
 var ASS_MANAGER = new AssetManager();
@@ -82,7 +31,21 @@ ASS_MANAGER.queueDownloads(
     'car.png',
     'cone-strawberry.png',
     'cone-vanilla.png'
-)
+);
+ASS_MANAGER.downloadAll(function() {
+    ROAD.src = ASS_MANAGER.getAsset('bg.png');
+    TRUCK.src = ASS_MANAGER.getAsset('truck.png');
+
+    CUSTOMERS.src_car = ASS_MANAGER.getAsset('car.png');
+
+    // Can start game once assets are loaded
+    start_game();   
+});
+
+// Create customers array
+var CUSTOMERS = new Customers();
+var TRUCK = new Truck();
+var ROAD = new Road();
 
 // Create the canvas
 var canvas = document.createElement("canvas");
@@ -91,20 +54,17 @@ canvas.width = game.w;
 canvas.height = game.h;
 document.body.appendChild(canvas);
 
-// Game objects
-var hero = {
-	speed: 256 // movement in pixels per second
-};
-var monster = {};
-var monstersCaught = 0;
-
-// Handle keyboard controls
-var keysDown = {};
-var keysUp = {};
 
 // for fps measurement
 var times = [];
 var fps;
+
+// /////////////////////////////////
+// // KEYBOARD HANDLING           //
+// /////////////////////////////////
+
+var keysDown = {};
+var keysUp = {};
 
 addEventListener("keydown", function (e) {
 	keysDown[e.keyCode] = true;
@@ -117,33 +77,33 @@ addEventListener("keyup", function (e) {
 
 
 // /////////////////////////////////
-// // UPDATE //
+// // UPDATE                      //
 // /////////////////////////////////
 
 var update = function (modifier) {
 
+    // Handle key presses
     if(38 in keysDown){         // UP
         delete keysDown[38];
-        truck.lane_change(-1);
+        TRUCK.change_lane(-1);
     }else if(40 in keysDown){   // DOWN
         delete keysDown[40];
-        truck.lane_change(1);
+        TRUCK.change_lane(1);
     }
 
+    // Update loops for objects
     CUSTOMERS.update_all(modifier);
-    road.update(modifier);
+    ROAD.update(modifier);
 
 };
 
 // /////////////////////////////////
-// // RENDER //
+// // RENDER                      //
 // /////////////////////////////////
 var render = function () {
-    ctx.drawImage(ASS_MANAGER.getAsset('cone-strawberry.png'), hero.x, hero.y);
-    ctx.drawImage(ASS_MANAGER.getAsset('cone-vanilla.png'), monster.x, monster.y);
 
-    road.render();
-    truck.render();
+    ROAD.render();
+    TRUCK.render();
     CUSTOMERS.render_all();
     
 	// FPS
@@ -155,7 +115,7 @@ var render = function () {
 };
 
 // /////////////////////////////////
-// // GAME LOOP //
+// // GAME LOOP                   //
 // /////////////////////////////////
 var main = function () {
 	var now = Date.now();
@@ -180,15 +140,10 @@ var main = function () {
 
 var then;
 
-// Let's play this game!
-// Load in assets
-ASS_MANAGER.downloadAll(function() {
-
-    road.src = ASS_MANAGER.getAsset('bg.png');
-    truck.src = ASS_MANAGER.getAsset('truck.png');
-
-    CUSTOMERS.src_car = ASS_MANAGER.getAsset('car.png');
-
+// /////////////////////////////////
+// // START                       //
+// ///////////////////////////////// 
+var start_game = function () {
     then = Date.now();
     main();
-});
+}
--- game/road.js
+++ game/road.js
@@ -1,39 +1,40 @@
 
-var road = {
-    src: null,
+function Road(){
+    this.src = null;
 
-    is_starting_up: true,  // Are we doing the initial acceleration or not
+    this.is_starting_up = true; // Are we doing the initial acceleration or not
 
-    speed: 200,           // current speed in pixels per second -> TODO: set to 0 for production
-    accel: 100,         // pixels per second^2
-    target_speed: 300,  // desired road speed
+    this.speed = 200;           // current speed in pixels per second -> TODO: set to 0 for production
+    this.accel = 100;           // pixels per second^2
+    this.target_speed = 300;    // desired road speed
 
-    pos: 0,
-    render: function(){
-        // Draw road, and one road behind
-        // TODO: Programmatically generate this terrain
-        ctx.drawImage(this.src,this.pos,0);
-        ctx.drawImage(this.src,this.pos+this.src.width,0);
-    },
-    update: function(modifier){
+    this.pos = 0;
+}
 
-        // Accelerate to desired speed (if necessary)
-        if(this.target_speed > this.speed){
-            this.speed += this.accel*modifier;
+Road.prototype.render = function(){
+    // Draw road, and one road behind
+    // TODO: Programmatically generate this terrain
+    ctx.drawImage(this.src,this.pos,0);
+    ctx.drawImage(this.src,this.pos+this.src.width,0);
+}
+
+Road.prototype.update = function(modifier){
+    // Accelerate to desired speed (if necessary)
+    if(this.target_speed > this.speed){
+        this.speed += this.accel*modifier;
+    }
+    if(this.speed > this.target_speed){
+        this.speed = this.target_speed;
+        if(this.is_starting_up){
+            // Finished starting up, so trigger something!
+            CUSTOMERS.add();
+            this.is_starting_up = false;
         }
-        if(this.speed > this.target_speed){
-            this.speed = this.target_speed;
-            if(this.is_starting_up){
-                // Finished starting up, so trigger something!
-                CUSTOMERS.add();
-                this.is_starting_up = false;
-            }
-        }
+    }
 
-        // Advance position of road
-        this.pos -= this.speed*modifier;
-        if(this.pos < -this.src.width){
-            this.pos = this.pos + this.src.width;
-        }
+    // Advance position of road
+    this.pos -= this.speed*modifier;
+    if(this.pos < -this.src.width){
+        this.pos = this.pos + this.src.width;
     }
 }
