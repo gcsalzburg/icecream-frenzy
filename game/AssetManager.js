c8ed0631b26eef421f674e60eec39df6ffeec6a8
gcsalzburg <g.cave@kiska.com>
2019-01-14 23:02:18 +0100

    Add in <audio> loading

--- game/AssetManager.js
+++ game/AssetManager.js
@@ -1,7 +1,7 @@
 // Taken from:
 // https://www.html5rocks.com/en/tutorials/games/assetmanager/
 
-function AssetManager(debug) {
+function AssetManager() {
     this.defaultPath = "";
     this.successCount = 0;
     this.errorCount = 0;
@@ -34,29 +34,58 @@ AssetManager.prototype.downloadAll = function(downloadCallback) {
         downloadCallback();
     }
     for (var i = 0; i < this.downloadQueue.length; i++) {
+
+        // Check type of downloaded file
         var path = this.downloadQueue[i];
-        var img = new Image();
-        var that = this;
-        img.addEventListener("load", function() {
-            if(that.debug){
-                console.log(this.src + ' is loaded');
-            }
-            that.successCount += 1;
-            if (that.isDone()) {
-                downloadCallback();
-            }
-        }, false);
-        img.addEventListener("error", function() {
-            if(that.debug){
-                console.log("ERROR:" + this.src + 'was NOT loaded');
-            }
-            that.errorCount += 1;
-            if (that.isDone()) {
-                downloadCallback();
-            }
-        }, false);
-        img.src = path;
-        this.cache[path] = img;
+        if(path.slice(-3) == "png"){
+            var img = new Image();
+            var that = this;
+            img.addEventListener("load", function() {
+                if(that.debug){
+                    console.log(this.src + ' is loaded');
+                }
+                that.successCount += 1;
+                if (that.isDone()) {
+                    downloadCallback();
+                }
+            }, false);
+            img.addEventListener("error", function() {
+                if(that.debug){
+                    console.log("ERROR:" + this.src + 'was NOT loaded');
+                }
+                that.errorCount += 1;
+                if (that.isDone()) {
+                    downloadCallback();
+                }
+            }, false);
+            img.src = path;
+            this.cache[path] = img;
+
+        }else if(path.slice(-3) == "mp3"){
+
+            var audio = new Audio();
+            audio.addEventListener('canplaythrough', function(){
+                if(that.debug){
+                    console.log(this.src + ' is loaded');
+                }
+                that.successCount += 1;
+                if (that.isDone()) {
+                    downloadCallback();
+                }
+            }, false);
+            audio.addEventListener('error', function(){
+                if(that.debug){
+                    console.log("ERROR:" + this.src + 'was NOT loaded');
+                }
+                that.errorCount += 1;
+                if (that.isDone()) {
+                    downloadCallback();
+                }
+            }, false);
+            audio.src = path;
+        }else{
+            console.log("Unknown file type: " + path);
+        }
     }
 }
 
