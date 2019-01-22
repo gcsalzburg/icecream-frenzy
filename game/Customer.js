2128f5168447ca32168a3fcaa4226800b72a6190
gcsalzburg <g.cave@kiska.com>
2019-01-22 21:48:22 +0100

    Add separate bubble rendering process

--- game/Customer.js
+++ game/Customer.js
@@ -1,4 +1,7 @@
-
+// /////////////////////////////////
+// // CUSTOMER                    //
+// // Each vehicle that arrives   //
+// /////////////////////////////////
 
 class Customer{
 
@@ -15,9 +18,29 @@ class Customer{
 
         this._dims = {
             lanes: [180,278,379,464],
-            bubble_segments: [18,26,17]
+            serving_pos: {
+                start: 114,
+                end: -121
+            }
+        }  
+        this._b_dims = {
+            segs: [18,26,17],
+            size: {
+                w: 61,
+                h: 56
+            },
+            offset: {
+                x: 25,
+                y: -20
+            },
+            mid_width: 7,      // starting width of the centre of bubble
+            mid_width_inc: 18, // each ice cream adds 18px to the width
+            ice_offset: {      // from bubble
+                x: 14,
+                y: 6
+            }
+
         }
-    
 
         // Create the order for this customer
         this.orders = [];
@@ -31,6 +54,9 @@ class Customer{
     isFed(){
         return this.is_fed;
     }
+    isServing(){
+        return ((this._pos > this._dims.serving_pos.end) && (this._pos < this._dims.serving_pos.start));
+    }
     getLane(){
         return this._lane;
     }
@@ -43,12 +69,53 @@ class Customer{
         this._src_ani.draw(elapsed,this._pos,this._dims.lanes[this._lane]);
     
         if(this._is_ordering & !this._is_fed){
-            ctx.drawImage(CUSTOMERS.src_speech_bubble,this._pos+25,this._dims.lanes[this._lane]-20);
-            for(var i=0; i<this.orders.length; i++){
-                var o = this.orders[i];
-                if(!o.is_served){
-                    ctx.drawImage(CUSTOMERS.src_cones[o.type],this._pos+33+(i*15),this._dims.lanes[this._lane]-15);
-                }
+            this.render_order();
+        }
+    }
+
+    render_order(){
+
+        var orders = 0;
+        for(var i=0; i<this.orders.length; i++){
+            if(!this.orders[i].is_served){
+                orders++;
+            }
+        }
+
+        // Bubble section widths and start co-ordinates
+        var widths = [
+            this._b_dims.segs[0],
+            this._b_dims.mid_width + ((orders-1)*this._b_dims.mid_width_inc),
+            this._b_dims.segs[2]
+        ];
+        var starts = [
+            0,
+            this._b_dims.segs[0],
+            this._b_dims.segs[0]+this._b_dims.segs[1]
+        ];
+        var src = (this.isServing() ? CUSTOMERS.src_bubbles[1] : CUSTOMERS.src_bubbles[0]);
+
+        // Draw three parts of the bubble
+        var width_so_far = 0;
+        for(var i=0; i<3; i++){
+            ctx.drawImage(
+                src,
+                starts[i],                                    0,                                                  this._b_dims.segs[i], this._b_dims.size.h, // Source
+                this._pos+this._b_dims.offset.x+width_so_far, this._dims.lanes[this._lane]+this._b_dims.offset.y, widths[i],            this._b_dims.size.h  // Destination
+            );  
+            width_so_far += widths[i];  
+        }
+        // Draw cones
+        var offset = 0;
+        for(var i=0; i<this.orders.length; i++){
+            var o = this.orders[i];
+            if(!o.is_served){
+                ctx.drawImage(
+                    CUSTOMERS.src_cones[o.type],
+                    this._pos + this._b_dims.offset.x + this._b_dims.ice_offset.x + offset,
+                    this._dims.lanes[this._lane] + this._b_dims.offset.y + this._b_dims.ice_offset.y
+                );
+                offset += this._b_dims.mid_width_inc;
             }
         }
     }
