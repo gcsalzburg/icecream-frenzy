3b5d2c125d1d3ca85c97976256a6f4af2340e8b1
gcsalzburg <g.cave@kiska.com>
2019-01-22 22:10:02 +0100

    Fix bug in non-looping frame calculation

--- game/Sprite.js
+++ game/Sprite.js
@@ -3,7 +3,7 @@
 
 
 class Sprite {
-    constructor(src, frames, cols, rows, framerate) {
+    constructor(src, frames, cols, rows, framerate, loop = true) {
         this.src    = src;
         this.frames = frames;
         this.cols   = cols;
@@ -11,12 +11,21 @@ class Sprite {
         this.w      = src.width/cols;
         this.h      = src.height/rows;  
 
-        this.framerate = framerate;     
+        this.framerate = framerate;   
+        
+        this.loop = loop; // boolean
     }
 
     draw(elapsed_ms,x,y) {
         // Calculate the frame of the animation
-        var curr_frame = Math.floor((elapsed_ms/this.framerate)%this.frames);
+        if(this.loop){
+            var curr_frame = Math.floor((elapsed_ms/this.framerate)%this.frames);
+        }else{
+            var curr_frame = Math.floor((elapsed_ms/this.framerate)/this.frames);
+            if(curr_frame >= this.frames){
+                curr_frame = this.frames-1;
+            }
+        }
 
         // Get co-ordinates for this sprite frame
         var x_c = Math.floor(curr_frame%this.cols)*this.w;
