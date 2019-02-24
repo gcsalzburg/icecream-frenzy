3bcdd5d6b15ca1c48d9f0320bd72423b113d4748
gcsalzburg <g.cave@kiska.com>
2019-02-24 18:13:07 +0100

    Add rotation parameter

--- game/EndCone.js
+++ game/EndCone.js
@@ -1,6 +1,6 @@
 
 class EndCone{
-    constructor(src, x, y, scale){
+    constructor(src, x, y, scale,rotation){
 
         this._src = src
         
@@ -11,6 +11,7 @@ class EndCone{
             scale: scale,
             x: x,
             y: y,
+            rotation: (rotation*Math.PI / 180),
 
             new_w: 321*scale,
             new_h: 302*scale
@@ -20,9 +21,14 @@ class EndCone{
 
     render(){
 
+        ctx.save();
+        ctx.translate(this._dims.x + (this._dims.new_w/2), this._dims.y + (this._dims.new_h/2));
+        ctx.rotate(this._dims.rotation);
         ctx.drawImage(
             this._src,
             0,0,this._dims.raw_w,this._dims.raw_h,
-            this._dims.x,this._dims.y,this._dims.new_w,this._dims.new_h);
+            -(this._dims.new_w/2),-(this._dims.new_h/2),this._dims.new_w,this._dims.new_h);
+        ctx.restore();
+    
     }
 }
